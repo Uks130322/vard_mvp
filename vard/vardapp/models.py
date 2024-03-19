49c3b92282aec586001312ebeedadda1896cf82a
Natalia Tkachuk <NLToshevikova@gmail.com>
2024-03-19 11:41:41 +0300

    add all models

--- vard/vardapp/models.py
+++ vard/vardapp/models.py
@@ -32,10 +32,10 @@ class UserManager(BaseUserManager):
 
 
 class Users(AbstractBaseUser, PermissionsMixin):
-    #id = models.AutoField(primary_key=True, blank=False, null=False, unique=True, verbose_name='user id')
+    # id = models.AutoField(primary_key=True, blank=False, null=False, unique=True, verbose_name='user id')
     name = models.CharField(max_length=255, verbose_name='name of user')
     email = models.EmailField(unique=True, max_length=255, verbose_name='email of user')
-    date_creation = models.DateTimeField(auto_now_add=True, verbose_name='date of dign up')
+    date_creation = models.DateTimeField(auto_now_add=True, verbose_name='date of sign up')
     date_password_change = models.DateTimeField(auto_now=True, verbose_name='date of password change')
     password = models.CharField(_("password"), max_length=255)
     is_staff = models.BooleanField(default=False)
@@ -48,13 +48,16 @@ class Users(AbstractBaseUser, PermissionsMixin):
     def __str__(self):
         return f'{self.email}'
 
+
 class Access(models.Model):
-    #id = models.AutoField(primary_key=True, blank=False, null=False, unique=True, verbose_name='access id')
+    # id = models.AutoField(primary_key=True, blank=False, null=False, unique=True, verbose_name='access id')
     user_id = models.ForeignKey('Users', on_delete=models.CASCADE, null=False, verbose_name='user id')
-    access_type_id = models.ForeignKey('AccessType', on_delete=models.CASCADE, null=False, verbose_name='access type id')
+    access_type_id = models.ForeignKey('AccessType', on_delete=models.CASCADE,
+                                       null=False, verbose_name='access type id')
     file_id = models.ForeignKey('Files', on_delete=models.CASCADE, verbose_name='')
-    date_access_open = models.DateTimeField(auto_now = True, verbose_name='date access open')
-    date_access_close = models.DateTimeField(auto_now = True, verbose_name='date access close')
+    date_access_open = models.DateTimeField(auto_now=True, verbose_name='date access open')
+    date_access_close = models.DateTimeField(auto_now=True, verbose_name='date access close')
+
 
 class AccessType(models.Model):
     p1 = 1
@@ -67,7 +70,8 @@ class AccessType(models.Model):
         (p3, 'COMMENTATOR'),
         (p4, 'EDITOR'),
     ]
-    access_type = models.IntegerField(choices = POSITIONS) #, default=p1
+    access_type = models.IntegerField(choices=POSITIONS)  # , default=p1
+
 
 class Files(models.Model):
     p0 = 0
@@ -76,16 +80,17 @@ class Files(models.Model):
         (p0, 'NO'),
         (p1, 'YES'),
     ]
-    #id = models.AutoField(primary_key=True, blank=False, null=False, unique=True, verbose_name='file id')
+    # id = models.AutoField(primary_key=True, blank=False, null=False, unique=True, verbose_name='file id')
     user_id = models.ForeignKey('Users', on_delete=models.CASCADE, null=False, verbose_name='user id')
     place_id = models.ForeignKey('Place', on_delete=models.CASCADE, null=False, verbose_name='id of place file')
     type_id = models.ForeignKey('FilesType', on_delete=models.CASCADE, null=False, verbose_name='id type of file')
-    date_creation = models.DateTimeField(auto_now_add = True, verbose_name='date of creation')
-    date_change = models.DateTimeField(auto_now = True, verbose_name='date of change')
-    date_delete = models.DateTimeField(auto_now = True, verbose_name='date of delete')
-    name = models.CharField(max_length = 255, verbose_name='name of file')
-    link = models.CharField(max_length = 255, verbose_name='link of file')
-    publish = models.IntegerField(choices = POSITIONS, default=p0)
+    date_creation = models.DateTimeField(auto_now_add=True, verbose_name='date of creation')
+    date_change = models.DateTimeField(auto_now=True, verbose_name='date of change')
+    date_delete = models.DateTimeField(auto_now=True, verbose_name='date of delete')
+    name = models.CharField(max_length=255, verbose_name='name of file')
+    link = models.CharField(max_length=255, verbose_name='link of file')
+    publish = models.IntegerField(choices=POSITIONS, default=p0)
+
 
 class Place(models.Model):
     p1 = 1
@@ -96,7 +101,8 @@ class Place(models.Model):
         (p2, 'My Files'),
         (p3, 'Best Practices'),
     ]
-    type = models.IntegerField(choices = POSITIONS) #, default=p1
+    type = models.IntegerField(choices=POSITIONS)  # , default=p1
+
 
 class FilesType(models.Model):
     p1 = 1
@@ -109,46 +115,47 @@ class FilesType(models.Model):
         (p2, 'JSON'),
         (p3, 'EXCEL'),
         (p4, 'PDF'),
-        (p5, '...'),
+        # (p5, '...'),
     ]
-    files_type = models.IntegerField(choices = POSITIONS) #, default=p1
+    files_type = models.IntegerField(choices=POSITIONS)  # , default=p1
+
 
 class Feedback(models.Model):
-    #id = models.AutoField(primary_key=True, blank=False, null=False, unique=True, verbose_name='feedback id')
+    # id = models.AutoField(primary_key=True, blank=False, null=False, unique=True, verbose_name='feedback id')
     user_id = models.ForeignKey(Users, on_delete=models.CASCADE, null=False, verbose_name='user id')
     date_creation = models.DateTimeField(auto_now_add=True, verbose_name='date of creation')
     theme = models.CharField(max_length=255, verbose_name='theme of feedback')
     description = models.TextField(verbose_name='feedback')
 
 
-class Dushboards(models.Model):
-    #id = models.AutoField(primary_key=True, blank=False, null=False, unique=True, verbose_name='dushboard id')
+class Dashboards(models.Model):
+    # id = models.AutoField(primary_key=True, blank=False, null=False, unique=True, verbose_name='dashboard id')
     user_id = models.ForeignKey('Users', on_delete=models.CASCADE, verbose_name='user id')
     date_creation = models.DateTimeField(auto_now_add=True, verbose_name='date of creation')
     date_change = models.DateTimeField(auto_now=True, verbose_name='date of change')
+
 
 class Charts(models.Model):
-    #id = models.AutoField(primary_key=True, blank=False, null=False, unique=True, verbose_name='charts id')
+    # id = models.AutoField(primary_key=True, blank=False, null=False, unique=True, verbose_name='charts id')
     user_id = models.ForeignKey('Users', on_delete=models.CASCADE, verbose_name='user id')
     date_creation = models.DateTimeField(auto_now_add=True, verbose_name='date of creation')
     date_change = models.DateTimeField(auto_now=True, verbose_name='date of change')
 
+
 class Comments(models.Model):
-    #id = models.AutoField(primary_key=True, blank=False, null=False, unique=True, verbose_name = 'publish id')
+    # id = models.AutoField(primary_key=True, blank=False, null=False, unique=True, verbose_name = 'publish id')
     file_id = models.ForeignKey('Files', on_delete=models.CASCADE, null=False, verbose_name='cells id')
     chart_id = models.ForeignKey('Charts', on_delete=models.CASCADE, null=False, verbose_name='chart id')
-    dushboard_id = models.ForeignKey('Dushboards', on_delete=models.CASCADE, null=False, verbose_name='dushboard id')
+    dashboard_id = models.ForeignKey('Dashboards', on_delete=models.CASCADE, null=False, verbose_name='dashboard id')
     user_id = models.ForeignKey('Users', on_delete=models.CASCADE, verbose_name='user id')
     date_send = models.DateTimeField(auto_now_add=True, verbose_name='date of send')
     date_remove = models.DateTimeField(auto_now_add=True, verbose_name='date of remove')
     date_delivery = models.DateTimeField(auto_now_add=True, verbose_name='date of delivery')
     comment = models.TextField(verbose_name='comment')
 
+
 class ReadComments(models.Model):
-    #id = models.AutoField(primary_key=True, blank=False, null=False, unique=True, verbose_name='id')
+    # id = models.AutoField(primary_key=True, blank=False, null=False, unique=True, verbose_name='id')
     comment_id = models.ForeignKey('Comments', on_delete=models.CASCADE, verbose_name='id of comment')
     user_id = models.ForeignKey('Users', on_delete=models.CASCADE, verbose_name='id of user')
     date_reading = models.DateTimeField(auto_now_add=True, verbose_name='date of read')
-
-
-
