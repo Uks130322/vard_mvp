b0301d79a73845135a134a09fd991423c0ee322d
ываппарпвпрвпывыа <stds58@gmail.com>
2024-04-08 14:23:59 +0300

    sql query api2

--- vard/appquery/models.py
+++ vard/appquery/models.py
@@ -1,15 +1,11 @@
 from django.db import models
 from vardapp.models import Users
-#import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql import text
 from sqlalchemy_utils import create_database, drop_database, database_exists
 from sqlalchemy import exc
 import re
-import json
-#import datetime
-
 
 
 class ClientDB(models.Model):
@@ -29,44 +25,6 @@ class ClientDB(models.Model):
     data_base_name = models.CharField(max_length=63,null=False)
     description = models.CharField(null=True, max_length=255)
     str_connection = models.CharField(blank=True, null=True, max_length=255)
-
-    # def get_host(self):
-    #     if self.host == 'localhost' or self.host == '127.0.0.1':
-    #         result = self.host
-    #     elif self.port == '' or self.port is None or not self.port:
-    #         result = f'{self.url}:3306'
-    #     else:
-    #         result = f'{self.url}:{self.port}'
-    #     return result
-    #
-    # @property
-    # def get_str_connect_sqlalchemy(self):
-    #     password_new = self.password.replace('@', '%40')
-    #     password_new = re.escape(password_new)
-    #     host_new = self.get_host()
-    #     str_connect = f"mysql://{self.user_name}:{password_new}@{host_new}/{self.data_base_name}"
-    #     return str_connect
-
-    # def get_engine_sqlalchemy(self, url, host, port, password, driver, user_name, data_base_name):
-    #     if driver == 1:
-    #         str_connect_new = self.get_str_connect_sqlalchemy(url, host, port, password, user_name, data_base_name)
-    #     else:
-    #         str_connect_new = ''
-    #     engine = create_engine(f"{str_connect_new}", echo=False)
-    #     return engine
-    #
-    # def get_query(self, user_id, url, host, port, password, driver, user_name, data_base_name, str_query):
-    #     engine = self.get_engine_sqlalchemy(url, host, port, password, driver, user_name, data_base_name)
-    #     Session = sessionmaker(autoflush=False, bind=engine)
-    #     with Session(autoflush=False, bind=engine) as db:
-    #         rows = db.execute(text(str_query)).fetchall()
-    #         data = [r._asdict() for r in rows]
-    #         keys_list = [set(r._asdict().keys()) for r in rows][0]
-    #         fieldname = []
-    #         for i in keys_list:
-    #             fieldname.append(i)
-    #         result = {'user_id': user_id, 'fieldname': fieldname, 'data': data, 'error': None}
-    #     return result
 
     def get_responces(self, id):
         try:
@@ -92,14 +50,7 @@ class ClientDB(models.Model):
             return result
 
     def update_responce(self, id):
-        # def datetime_handler(x):
-        #     if isinstance(x, datetime.datetime):
-        #         return x.isoformat()
-        #     raise TypeError("Unknown type")
-        user = Users.objects.get(id=self.user.id)
         result = self.get_responces(id)
-        # json_string = json.dumps(result, default=datetime_handler)
-        #ClientDB.objects.filter(id=id).update(result_query=result)
         return result
 
     def __str__(self):
@@ -107,7 +58,6 @@ class ClientDB(models.Model):
 
 
 class Chart(models.Model):
-    # id = models.AutoField(primary_key=True, blank=False, null=False, unique=True, verbose_name='chart id')
     user_id = models.ForeignKey(Users, on_delete=models.CASCADE, verbose_name='user id')
     date_creation = models.DateTimeField(auto_now_add=True, verbose_name='date of creation')
     date_change = models.DateTimeField(auto_now=True, verbose_name='date of change')
@@ -121,94 +71,4 @@ class Chart(models.Model):
 class ClientData(models.Model):
     chart = models.OneToOneField(Chart, on_delete=models.CASCADE)
     data = models.TextField(blank=True, null=True)
-
-
-#--------------------------------------
-class DataBaseQuery(models.Model):  #DataBaseQuery  ClientData
-    def __init__(self, user_id, driver, user_name, password, url,host,port, data_base_name, data_base_type, description, str_query):
-        self.user_id = user_id
-        self.driver = driver
-        self.user_name = user_name
-        self.password = password
-        self.url = url
-        self.host = host
-        self.port = port
-        self.data_base_name = data_base_name
-        self.data_base_type = data_base_type
-        self.description = description
-        self.str_query = str_query
-
-    def get_host(self):
-        """
-            сначала подключаемся к локальному хосту. поэтому сначала проверяем на валидность локальный хост, если он невалиден,
-            проверяем порт и подключаемся к удалённому url.
-            у mysql по умолчанию порт = 3306. если порт не указан, устанавливаем 3306.
-            эта проверка нужна тк в result-е стоит двоеточие : f'{self.url}:{self.port}
-            если url неправилен, то бд вернёт ошибку,т.е. дополнительно его проверять не нужно
-            """
-        if self.host == 'localhost' or self.host == '127.0.0.1':
-            result = self.host
-        elif self.port == '' or self.port is None or not self.port:
-            result = f'{self.url}:3306'
-        else:
-            result = f'{self.url}:{self.port}'
-        return result
-
-    def get_str_connect(self):
-        password_new = self.password.replace('@', '%40')
-        password_new = re.escape(password_new)
-        host_new = self.get_host()
-        str_connect = f"{self.driver}://{self.user_name}:{password_new}@{host_new}/{self.data_base_name}"
-        return str_connect
-
-    def get_engine(self):
-        str_connect_new = self.get_str_connect()
-        engine = create_engine(f"{str_connect_new}", echo=False)
-        return engine
-
-    def create_data_base(self):
-        str_connect_new = self.get_str_connect()
-        if not database_exists(f"{str_connect_new}"):
-            create_database(f"{str_connect_new}")
-            return f'{self.data_base_name} создана'
-        else:
-            return f'{self.data_base_name} уже существует'
-
-    def drop_data_base(self):
-        str_connect_new = self.get_str_connect()
-        if database_exists(f"{str_connect_new}"):
-            drop_database(f"{str_connect_new}")
-            return f'{self.data_base_name} удалена'
-        else:
-            return f'{self.data_base_name} не найдена'
-
-    def get_query(self):
-        engine = self.get_engine()
-        Session = sessionmaker(autoflush=False, bind=engine)
-        with Session(autoflush=False, bind=engine) as db:
-            rows = db.execute(text(self.str_query)).fetchall()
-            user = [{'user_id':self.user_id}]
-            result = user + [r._asdict() for r in rows]
-        return result
-
-    @property
-    def get_responces(self):
-        try:
-            if not self.str_query or self.str_query is None or self.str_query == '':
-                result = [{'user_id': self.user_id}]
-            else:
-                result = self.get_query()
-            return result
-        except exc.SQLAlchemyError as e:
-            error = str(e.__dict__['orig'])
-            return [{'user_id': self.user_id}, {'error': f'{error}'}]
-
-
-
-
-
-
-
-
-
 
--- vard/vard/urls.py
+++ vard/vard/urls.py
@@ -1,29 +1,12 @@
-"""
-URL configuration for vard project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers, serializers, viewsets
+from rest_framework import routers
 from appquery import views
 from vardapp import views as views2
 
 
 router = routers.DefaultRouter()
-#router.register(r'dbview', views.DBView, basename='dbview')
-#router.register(r'client', views.ClientViewset)
 router.register(r'users', views2.UserViewset)
 router.register(r'charts', views.ChartViewSet)
 router.register(r'clientdb', views.ClientDBViewSet)
@@ -34,7 +17,6 @@ urlpatterns = [
     path('api/', include(router.urls)),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('xxx/', include(('appquery.urls', 'appquery'), namespace='appquery')),
     path('drf/', include('rest_framework.urls', namespace='rest_framework')),
 
 
