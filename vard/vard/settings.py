1fddc9afe3bd928ea5df1a1a459ab3428854ba67
Grasmurr <grasmurr@gmail.com>
2024-07-30 18:31:11 +0300

    added localhost as allowed_host

--- vard/vard/settings.py
+++ vard/vard/settings.py
@@ -172,6 +172,7 @@ DATABASES = {
         'HOST': 'db',
         'PORT': '5432',
     }
+}
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
 #         'NAME': BASE_DIR / 'db.sqlite3',
