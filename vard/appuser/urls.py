ce4a7855e6a9d9f1ade6051c544ec1b13f58c438
Natalia Tkachuk <101522861+Uks130322@users.noreply.github.com>
2024-08-07 21:34:25 +0300

    Api urls (#39)
    * add all api links on api root
    
    * add all api links on api root, final fixes
    
    * comment docker test db

--- vard/appuser/urls.py
+++ vard/appuser/urls.py
@@ -7,11 +7,8 @@ from appuser import views
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'access', views.AccessViewSet)
-router.register(r'authregister', views.RegisterView, basename='authregister')
 
 urlpatterns = [
     path('', include(router.urls)),
     path('api/', views.FlatpageView, name= 'FlatpageView'),
-
 ]
-
--- vard/vard/urls.py
+++ vard/vard/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from dj_rest_auth.views import LoginView, LogoutView, UserDetailsView
-from allauth.socialaccount.views import signup
 from rest_framework import permissions, routers
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 from appuser.views import RegisterView, GoogleLogin, GitHubLogin, FlatpageView, UserViewSet, AccessViewSet
+from appuser.auth_router import DefaultRouterWithSimpleViews
 
 from appchart_DB.urls import router as appchart_DBrouter
 from appchat.urls import router as appchatrouter
@@ -30,28 +30,27 @@ schema_view = get_schema_view(
 )
 
 
-router = routers.DefaultRouter()
+router = DefaultRouterWithSimpleViews()
 router.registry.extend(appchart_DBrouter.registry)
 router.registry.extend(appchatrouter.registry)
 router.registry.extend(appcommentrouter.registry)
 router.registry.extend(appfeedbackrouter.registry)
 router.registry.extend(appfilerouter.registry)
 router.registry.extend(appuserrouter.registry)
+router.register(r'register',RegisterView, basename="register")
+router.register(r'login', LoginView, basename="login")
+router.register(r'logout', LogoutView, basename="logout")
+router.register(r'user', UserDetailsView, basename="user_details")
+router.register(r'google_login', GoogleLogin, basename="google_login")
+router.register(r'github_login', GitHubLogin, basename="github_login")
 
 urlpatterns = [
-    path("api/auth/signup/", signup, name="socialaccount_signup"), # нет в свагере
-    path("api/auth/google/", GoogleLogin.as_view(), name="google_login"),
-    path('api/auth/github/', GitHubLogin.as_view(), name='github_login'),
-    path("api/auth/login/", LoginView.as_view(), name="rest_login"),
-    path("api/auth/logout/", LogoutView.as_view(), name="rest_logout"),
     path('admin/', admin.site.urls), # нет в свагере
     path('api/accounts/', include('allauth.urls')), # нет в свагере
-    path("api/auth/user/", UserDetailsView.as_view(), name="rest_user_details"),
-    path('api/', include(router.urls)), ### ! новые пути не забываем добавлять в templates/rest_framework/base.html  !
-    path('apilist/', FlatpageView, name="api"),  # нет в свагере ### ! новые пути не забываем добавлять в defaults/default_api.html  !
+
+    path('api/', include(router.urls)),
+
     path('drf/', include('rest_framework.urls', namespace='rest_framework')), # нет в свагере # не работает путь
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'), # нет в свагере
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'), # нет в свагере
-
 ]
-
